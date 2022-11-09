5e59025241079e40a4df680cda11067cd74344cc
Shubham Nigam <apple@Apples-MacBook-Pro.local>
2022-11-09 22:11:27 +0530

    project structure ready...

---
+++ README.md
@@ -0,0 +1,1 @@
+# springboot-fullstack-react-blogging-app
---
+++ springboot-fullstack-react-bloggingapp/src/test/java/com/daadestroyer/springbootfullstackreactbloggingapp/SpringbootFullstackReactBloggingappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daadestroyer.springbootfullstackreactbloggingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFullstackReactBloggingappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
